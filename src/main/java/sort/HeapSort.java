bdcdbe40b26f0b86bcc0278b959dc540303a480c
se7endorycodervicky <99240451+se7endorycodervicky@users.noreply.github.com>
2022-03-21 11:42:01 +0530

    Update HeapSort.java

--- src/main/java/sort/HeapSort.java
+++ src/main/java/sort/HeapSort.java
@@ -1,10 +1,16 @@
 package sort;
 
+/**
+This program can be used for following. Below code will implement a max. heap
+
+1. Sort an array using heap sort
+2. Find Kth minimum from an array
+**/
 public class HeapSort {
 
     public void sort(int arr[]) {
 
-        for (int i = 0; i < arr.length /2; i++) {
+        for (int i = 0; i < arr.length /2; i++) {   
             heapify(arr, arr.length, i);
         }
 
@@ -16,19 +22,32 @@ public class HeapSort {
             heapify(arr, i, 0);
         }
 
+        //For Kth's element from min heap
+        /*
+        for (int i = K /2 -1 ; i >= 0; i--) {
+            heapify(arr, K, i);        
+        }
+
+        for (int i = K;  i < arr.length; i++) {
+            if ( arr[i] > arr[0]) {
+                arr[0] = arr[i];
+                heapify(arr, K, 0);
+            }
+        }
+        */
 
     }
 
     public void heapify(int arr[], int n, int i ) {
-        int largest = i;
+        int largest = i;                  
         int left = 2*i +1;
         int right = 2*i + 2;
 
-        if ( left < n && arr[left] > arr[largest]) {
+        if ( left < n && arr[left] > arr[largest]) {      //for min heap, check for minimum
             largest = left;
         }
 
-        if  ( right < n && arr[right] > arr[largest]) {
+        if  ( right < n && arr[right] > arr[largest]) {    //for min heap, check for minimum
             largest = right;
         }
 
@@ -50,6 +69,7 @@ public class HeapSort {
         for (int i  = 0; i < arr.length; i++) {
             System.out.print("  " +  arr[i]);
         }
+        //if we want to get Kth minimum, access arr[k]
     }
 
 }
