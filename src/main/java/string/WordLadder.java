c9805946cf47f06318fc7cf0750a0def80baf800
se7endorycodervicky <99240451+se7endorycodervicky@users.noreply.github.com>
2022-07-10 17:14:01 +0530

    Update WordLadder.java

--- src/main/java/string/WordLadder.java
+++ src/main/java/string/WordLadder.java
@@ -1,102 +1,132 @@
 package string;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
 /*
- Given a dictionary, and two words ‘start’ and ‘target’ (both of same length).
- Find length of the smallest chain from ‘start’ to ‘target’ if it exists, such that
- adjacent words in the chain only differ by one character and each word in the chain is a valid word
+A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
 
- i.e., it exists in the dictionary.
- It may be assumed that the ‘target’ word exists in dictionary and length of all dictionary words is same.
+Every adjacent pair of words differs by a single letter.
+Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+sk == endWord
+Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+
+Example:
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+Output: 5
+Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+
  */
-
-import java.util.*;
-
 public class WordLadder {
+
     static class Word {
-        String word;
+        String str;
         int level;
 
-        public Word(String word, int level) {
-            this.word = word;
+        public Word(String str, int level) {
+            this.str = str;
             this.level = level;
         }
     }
 
-    public static void ChainLen(String start, String end, Set<String> dict) {
+    public static void getWordLadder(String start, String end, List<String> wordList) {
+        List<String> shortestLadder = null;
 
+        Map<Integer, String> wordLadder = new HashMap<>();
 
-        List<Word> queue = new LinkedList<>();
+        Stack<Word> stack = new Stack<>();
+        stack.add(new Word(start,0));
 
-        List<Word> usedWords = new LinkedList<>();
-
-        queue.add(new Word(start,1));
-
-        while ( queue.size() > 0) {
-
-
-            Word queueWord = queue.remove(0);
-            String currWord = queueWord.word;
-            System.out.println(">> " + currWord);
-
-            if ( currWord.equals(end)) {
-                System.out.println("Found Match at Level:" + queueWord.level);
-                return;
-            }
-
-            List<String> possibleWords = new ArrayList<String>();
-            for (int i = 0; i < currWord.length(); i++) {
-                String word = "";
-                for (int j = 0; j < currWord.length(); j++) {
-                    if ( i == j) {
-                        word += "*";
-                    } else word += currWord.charAt(j)+"";
+        while (! stack.isEmpty()) {
+            Word word = stack.pop();
+            if ( word.str.equals(end)) {
+                if ( wordLadder.size() > word.level) {
+                    for (int i = word.level+1; i < wordLadder.size(); i++) {
+                        wordLadder.remove(i);
+                    }
                 }
-                possibleWords.add(word);
+                if ( shortestLadder == null || wordLadder.size() < shortestLadder.size()) {
+                    if ( shortestLadder == null) shortestLadder = new LinkedList<>();
+                    shortestLadder.clear();
+                    for ( String val : wordLadder.values()) {
+                        shortestLadder.add(val);
+                    }
+                    shortestLadder.add(end);
+                }
+                continue;
             }
 
+            wordLadder.put(word.level, word.str);
 
-            for ( String word : possibleWords) {
-                String matched = isMatch(word, queueWord.level, queue, dict);
-                if ( matched == null) continue;
-                usedWords.add(new Word(matched, queueWord.level+1));
-                queue.add(new Word(matched, queueWord.level+1));
+            int currLevel = word.level;
+            //Get alternatives to word
+            List<String> possiblePathWords = new ArrayList<>();
+            for ( String dictWord : wordList) {
+                boolean abort = false;
+                for (int level = 0; level <= currLevel; level++) {
+                    if (wordLadder.get(level).equals(dictWord)) {
+                        abort = true;
+                        break;
+                    }
+                }
+                if ( abort) continue;
 
+                for (int i = 0; i < dictWord.length(); i++) {
+                    String pattern = word.str.substring(0, i) + "*" + word.str.substring(i + 1, word.str.length());
+                    if (isMatch(pattern, dictWord)) {
+                        possiblePathWords.add(dictWord);
+                    }
+                }
+
+            }
+
+            for ( String possibleWord : possiblePathWords) {
+                stack.add(new Word(possibleWord, currLevel+1));
             }
 
         }
 
-        System.out.println("<NO-MATCH>");
+
+        System.out.println("Shorted Word Path has : " + ((shortestLadder == null) ? 0 : shortestLadder.size()) + " words");
+        if ( shortestLadder != null) {
+            for (String str : shortestLadder) {
+                System.out.print(" " + str);
+            }
+        }
+
     }
 
-    public static String isMatch(String word, int level, List<Word> queue,Set<String> dict) {
-        for ( String dictWord : dict) {
-            for ( Word wrd : queue) {
-                if ( wrd.word.equals(dictWord) && wrd.level < level) continue; //This word is already used
-            }
+    public static boolean isMatch(String word, String dictWord) {
 
-            int dictWordIndex = 0;
-            boolean abort = false;
-            for (int wordIndex = 0; wordIndex < word.length(); wordIndex++, dictWordIndex++) {
-                if ( word.charAt(wordIndex) != '*'  && word.charAt(wordIndex) != dictWord.charAt(dictWordIndex)) {
-                    abort = true;
-                    break;
-                }
-            }
-            if ( !abort ) return dictWord;
+        for (int i = 0,  j = 0;  i < word.length(); i++, j++) {
+            if ( word.charAt(i) == '*' ) continue;
+            if ( word.charAt(i) != dictWord.charAt(i)) return false;
         }
-        return null;
+        return true;
     }
 
     public static void main(String args[]) {
-        Set<String> D = new HashSet<String>();
-        D.add("poon");
-        D.add("plee");
-        D.add("same");
-        D.add("poie");
-        D.add("plie");
-        D.add("poin");
-        D.add("plea");
-        String start = "toon";
-        String target = "plea";
-        ChainLen(start, target, D);
+        List<String> wordList = Arrays.asList(new String [] {"hot", "dot", "dog", "lot", "log", "cog"});
+        getWordLadder( "hit", "cog", wordList);   // hit hot lot log cog
+
+        System.out.println("\n");
+        wordList = Arrays.asList(new String [] {"hot", "dot", "dog", "lot", "log"});
+        getWordLadder( "hit", "cog", wordList);   // NO RESULT
+
+        System.out.println("\n");
+        wordList = Arrays.asList(new String [] {"POON", "PLEE", "SAME", "POIE", "PLEA", "PLIE", "POIN"});
+        getWordLadder( "TOON", "PLEA", wordList);   // TOON POON POIN POIE PLIE PLEE PLEA
+
+        System.out.println("\n");
+        wordList = Arrays.asList(new String [] {"ABCD", "EBAD", "EBCD", "XYZA"});
+        getWordLadder( "ABCV", "EBAD", wordList);   // ABCV ABCD EBCD EBAD
+
+
     }
+
 }
