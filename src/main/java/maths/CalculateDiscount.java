bf06367e45310c876b054ee52df7216c7ff8c64f
se7endorycodervicky <pradvicky@gmail.com>
2022-07-14 21:16:08 +0530

    desc added

--- src/main/java/maths/CalculateDiscount.java
+++ src/main/java/maths/CalculateDiscount.java
@@ -1,4 +1,10 @@
 package maths;
+/*
+Problem: Given the array prices where prices[i] is the price of the i'th item in a shop.
+There is a special discount for items in the shop, if you buy the i'th item,
+then you will receive a discount equivalent to prices[j] where j is the minimum index such that j > i and prices[j] <= prices[i],
+otherwise, you will not receive any discount at all.
+ */
 
 public class CalculateDiscount {
 
